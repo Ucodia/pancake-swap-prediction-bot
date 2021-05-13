ec94368671192f8ff17dbffaf494151eb343febf
Lionel Ringenbach <ucodia@live.com>
2021-05-12 23:48:06 -0700

    Added market status checker

---
+++ status.js
@@ -0,0 +1,25 @@
+const { contract } = require("./config");
+const { postToDiscord } = require("./discord");
+
+(async () => {
+  const startTime = new Date();
+  let previousStatus = true;
+
+  const pollMarketStatus = async () => {
+    const paused = await contract.methods.paused().call();
+
+    if (!paused && paused !== previousStatus) {
+      postToDiscord("Prediction market just re-opened! 🔮✨");
+    } else {
+      console.log(
+        startTime,
+        "Prediction market is still paused, refreshing in 5 minutes..."
+      );
+      setTimeout(pollMarketStatus, 5 * 1000);
+    }
+
+    previousStatus = paused;
+  };
+
+  pollMarketStatus();
+})();
